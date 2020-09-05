d2a618c9a00bac8dd1151861ca3ae965bca2212a
ryotaro suda <search_electron@protonmail.com>
2020-09-05 16:44:59 +0900

    1.1.0 dfs_mono lambda

--- binary_search.cpp
+++ binary_search.cpp
@@ -8,71 +8,53 @@
 const ll MOD = 1000000007;
 const ll INF = 1061109567;
 
-
-template <class T>
-ll binary_search(const T& binary_key, const std::vector<T>& binary_vec)
-{
-    //libproton 1.0.0
-    //鍵値とvectorを入力されたら鍵値より大きいか等しいギリギリのindexをlong longで返す
-    //もし反対が良ければbinary_search_rev()を使ってください
-    //vectorは必ず昇順でsortしてから入力すること,Tには鍵値,vectorの型名を代入
-    //binary_search<ll>(binary_key,binary_vec);
-    //libproton 1.0.1
-    //全部鍵値を下回るとsize()が出力されてstd::out_of_rangeするのでこの場合size()-1を出力します
-    ll bin_ok = (ll)binary_vec.size();
-    ll bin_ng = -1;
-    while (std::abs(bin_ok - bin_ng) > 1) {
-        ll bin_mid = (bin_ok + bin_ng) / 2;
-        if (binary_vec[bin_mid] >= binary_key)
-            bin_ok = bin_mid;
-        else
-            bin_ng = bin_mid;
-    }
-    if (bin_ok == binary_vec.size())
-        --bin_ok;
-    return bin_ok;
-}
-
-template <class T>
-ll binary_search_rev(const T& binary_key, const std::vector<T>& binary_vec)
-{
-    //libproton 1.0.0
-    //鍵値とvectorを入力されたら鍵値より小さいかと等しいギリギリのindexをlong longで返す
-    //もし反対が良ければbinary_search()を使ってください
-    //vectorは必ず昇順でsortしてから入力すること,Tには鍵値,vectorの型名を代入
-    //binary_search_rev<ll>(binary_key,binary_vec);
-    //libproton 1.0.1
-    //全部鍵値を上回ると-1が出力されてstd::out_of_rangeするのでこの場合0を出力します
-    ll bin_ok = -1;
-    ll bin_ng = (ll)binary_vec.size();
-    while (std::abs(bin_ok - bin_ng) > 1) {
-        ll bin_mid = (bin_ok + bin_ng) / 2;
-        if (binary_vec[bin_mid] <= binary_key)
-            bin_ok = bin_mid;
-        else
-            bin_ng = bin_mid;
-    }
-    if (bin_ok == -1)
-        ++bin_ok;
-    return bin_ok;
-}
-
-
 int main()
 {
-    ll n, m;
-    std::vector<ll> k;
-    std::cin >> n >> m;
-    for (ll i = 0; i < n; ++i) {
-        ll temp;
-        std::cin >> temp;
-        k.push_back(temp);
-    }
-    std::sort(k.begin(), k.end());
-    Say(binary_search<ll>(m, k));
-    Say(binary_search_rev<ll>(m, k));
+    auto binary_search = [&](const auto& binary_key, const auto& binary_vec) {
+        ll bin_ok = (ll)binary_vec.size();
+        ll bin_ng = -1;
+        while (std::abs(bin_ok - bin_ng) > 1) {
+            ll bin_mid = (bin_ok + bin_ng) / 2;
+            if (binary_vec[bin_mid] >= binary_key)
+                bin_ok = bin_mid;
+            else
+                bin_ng = bin_mid;
+        }
+        if (bin_ok == (ll)binary_vec.size())
+            --bin_ok;
+        return bin_ok;
+    };
+
+    auto binary_search_rev = [&](const auto& binary_key, const auto& binary_vec) {
+        ll bin_ok = -1;
+        ll bin_ng = (ll)binary_vec.size();
+        while (std::abs(bin_ok - bin_ng) > 1) {
+            ll bin_mid = (bin_ok + bin_ng) / 2;
+            if (binary_vec[bin_mid] <= binary_key)
+                bin_ok = bin_mid;
+            else
+                bin_ng = bin_mid;
+        }
+        if (bin_ok == -1)
+            ++bin_ok;
+        return bin_ok;
+    };
     return 0;
 }
+//libproton 1.1.0 binary_search()
+//鍵値とvectorを入力されたら鍵値より大きいか等しいギリギリのindexをlong longで返す
+//もし反対が良ければbinary_search_rev()を使ってください
+//binary_vecは必ず昇順でsortして,型をbinary_keyに合わせたstd::vectorかstd::arrayで入力すること,
+//binary_search(binary_key,binary_vec);
+//全部鍵値を下回るとsize()が出力されてstd::out_of_rangeするのでこの場合size()-1を出力します
+
+//libproton 1.1.0 binary_search_rev()
+//鍵値とvectorを入力されたら鍵値より大きいか等しいギリギリのindexをlong longで返す
+//もし反対が良ければbinary_search()を使ってください
+//binary_vecは必ず昇順でsortして,型をbinary_keyに合わせたstd::vectorかstd::arrayで入力すること,
+//binary_search_rev(binary_key,binary_vec);
+//全部鍵値を上回ると-1が出力されてstd::out_of_rangeするのでこの場合0を出力します
+
 /*
 https://qiita.com/drken/items/97e37dd6143e33a64c8c
 を参考にしただけです
---
+++ dfs_mono.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define Yes() std::cout << "Yes" << std::endl
+#define No() std::cout << "No" << std::endl
+#define Say(s) std::cout << s << std::endl
+const ll MOD = 1000000007;
+const ll INF = 1061109567;
+// 頂点のindexが1変数で表せるグラフなら使えます
+int main()
+{
+    using Graph = std::vector<std::vector<ll>>;
+    auto make_graph = [&](const ll& num_points, const ll& num_sides, const auto& side_pairs) {
+        Graph G(num_points);
+        for (ll i = 0; i < num_sides; ++i) {
+            G[side_pairs.at(i).at(0)].push_back(side_pairs.at(i).at(1));
+            //G[side_pairs.at(i).at(1)].push_back(side_pairs.at(i).at(0)); //有向グラフ
+        }
+        return G;
+    };
+    std::vector<bool> graph_seen(100, false);  //100には頂点数を入れる。下のdfsは再帰なので外でdfsを定義する必要あり。
+    std::function<void(const Graph&, const ll&, std::vector<bool>&)> dfs_mono = [&](const Graph& dfs_G, const ll& start_point, std::vector<bool>& dfs_seen) {
+        dfs_seen[start_point] = true;             // start_point を訪問済にする
+        for (auto next_v : dfs_G[start_point]) {  // start_point から行ける各頂点 next_v について
+            if (dfs_seen[next_v])
+                continue;                       // next_v が探索済だったらスルー
+            dfs_mono(dfs_G, next_v, dfs_seen);  // 再帰的に探索
+        }
+    };
+    return 0;
+}
+//libproton 1.1.0 make_graph
+//num_pointsは頂点数,num_sidesは辺数,side_pairsはvector::vector<ll>に相当するものを入力してください。
+//ただし二つ目のvectorは二つの要素しかないことを想定している
+//グラフ型(G[index]に頂点indexから直接行ける点の列挙が入っている)を返す
+//graphのiteratorが1次元の時しか対応していない
+
+//libproton 1.1.0  dfs_mono
+//graphのiteratorが1次元の時しか対応していない
+//dfs_GにはGraph型を, start_pointには探索開始点を, dfs_seenには頂点数の大きさを持った配列を入れてください。
+//返す値はない。dfs_seenが訪問済みでtrueに切り替わっているはず。やりたい動作は使用時に関数中に追記してください。
